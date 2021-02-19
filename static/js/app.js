a3957f7f376f40ae082c8204c3f62ca7b7285b45
Connor MacKenzie <cmm224@case.edu>
2021-02-18 20:38:36 -0500

    filter on button click

--- static/js/app.js
+++ static/js/app.js
@@ -2,25 +2,56 @@
 var tableData = data;
 
 // YOUR CODE HERE
-
-
-
-
-
-
-
-
-
-
-
-
 //the table creation!
-var table = d3.select('tbody');
+//making a function so that when we filter we can call it again
+function createTable(data){
+    // find the table body
+    var table = d3.select('tbody');
 
-tableData.forEach(sighting => {
-    var row = table.append('tr');
-    Object.entries(sighting).forEach(([key,value]) => {
-        var cell = row.append('td');
-        cell.text(value);
+    // clear any contents that might be in the table 
+    d3.selectAll('tbody>tr').remove();
+
+    // looping through the data and adding it to the table
+    data.forEach(sighting => {
+        var row = table.append('tr');
+        Object.entries(sighting).forEach(([key,value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
     });
+};
+
+createTable(tableData);
+
+//filter function
+
+//date filter stuff
+function UFOfilter(onWhat,value){
+    var newData = [];
+    tableData.forEach(sighting => {
+        if(sighting[onWhat] == value){
+            newData.push(sighting);
+        }
+    });
+    createTable(newData);
+};
+
+
+var dateField = d3.select('#datetime');
+var dateButton = d3.select('.button1');
+
+dateButton.on('click',function(){
+    d3.event.preventDefault();
+    UFOfilter('datetime', dateField.property('value'));
 });
+
+
+
+
+
+
+
+
+
+
+
