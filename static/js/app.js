a9099b867a4b46ac6fccf790630e3dfb4c955a2a
Connor MacKenzie <cmm224@case.edu>
2021-02-18 22:13:14 -0500

    added all the filter fields, adding a filter works. working on removing a filter when field is cleared

--- static/js/app.js
+++ static/js/app.js
@@ -29,11 +29,35 @@ createTable(tableData);
 // making an array to store what filters need to be applied
 var filters = [];
 
+//function to put things into filters, takes in what column needs filtered, and the field for that filter
+function addFilter(what,field){
+
+    //we only want to add it if the field contains a value
+    if (field.property('value') != ''){
+
+        //make sure the value isn't already in there, and then push if we should
+        var push = true;
+        filters.forEach(([filterwhat,field]) =>{
+            if (filterwhat == what){
+                push = false;
+            };
+        });
+        if (push){
+            filters.push([what, field]);
+        };
+    }
+};
+
+
 //variables for the form, each field, and the button
 var Form = d3.select('form')
 
 var dateField = d3.select('#datetime');
-var cityField = d3.select('#city')
+var cityField = d3.select('#city');
+var stateField = d3.select('#state');
+var countryField = d3.select('#country');
+var shapeField = d3.select('#shape');
+
 
 var Button = d3.select('button');
 
@@ -41,51 +65,72 @@ var Button = d3.select('button');
 //filter function
 
 function UFOfilter(){
-    filters.forEach(([what,field]) =>{
-        var value = field.property('value');
-        var newData = [];
-        tableData.forEach(sighting => {
-            if(sighting[what] == value){
-                newData.push(sighting);
-            };
+    if (filters.length == 0){
+        createTable(tableData);
+    }
+    else{
+        filters.forEach(([what,field]) =>{
+            var value = field.property('value');
+            var newData = [];
+            tableData.forEach(sighting => {
+                if(sighting[what] == value){
+                    newData.push(sighting);
+                };
+            });
+            createTable(newData);
         });
-        createTable(newData);
-    });
+    };
 };
 
 
-
+//don't do anything if they just hit enter in a field. might change this later
 Form.on('submit',function(){
     d3.event.preventDefault();
 });
 
+//for every field that changes, we make sure that it will get filtered if the field isn't empty
+
 dateField.on('change',function(){
     console.log(filters);
     d3.event.preventDefault();
-    if (d3.event.target.value != ''){
-        if (!('datetime' in Object.keys(filters))){
-            filters.push(['datetime', dateField]);
-        };
-    };
+    addFilter('datetime',dateField);
+    
 });
 
 cityField.on('change',function(){
     console.log(filters);
     d3.event.preventDefault();
-    if (d3.event.target.value != ''){
-        if (!('city' in Object.keys(filters))){
-            filters.push(['city', cityField]);
-        };
-    };
+    addFilter('city',cityField)
+});
+
+stateField.on('change',function(){
+    console.log(filters);
+    d3.event.preventDefault();
+    addFilter('state',stateField)
+});
+
+countryField.on('change',function(){
+    console.log(filters);
+    d3.event.preventDefault();
+    addFilter('country',countryField)
+});
+
+shapeField.on('change',function(){
+    console.log(filters);
+    d3.event.preventDefault();
+    addFilter('shape',shapeField)
 });
 
 
+//when the button is clicked, filters the data
 Button.on('click',function(){
     d3.event.preventDefault();
     UFOfilter();
 });
 
-
+//trying something else
+var list = d3.selectAll('li');
+console.log(list);
 
 
 
