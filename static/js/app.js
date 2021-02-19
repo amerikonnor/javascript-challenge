9ae57e212f8d9a4f63fe294620ff1d9b00b3b44e
Connor MacKenzie <cmm224@case.edu>
2021-02-18 20:07:58 -0500

    table insertion works

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// YOUR CODE HERE
+
+
+
+
+
+
+
+
+
+
+
+
+//the table creation!
+var table = d3.select('tbody');
+
+tableData.forEach(sighting => {
+    var row = table.append('tr');
+    Object.entries(sighting).forEach(([key,value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
